82e3558f8d6bf4fd64f8a482d9a1d7a87e8f795f
ylewisbedford <yobedford@gmail.com>
2023-03-29 13:38:34 +0100

    javascript being added and test on functionality of buttons

---
+++ button.js
@@ -0,0 +1,11 @@
+let latestButton = document.getElementById("latestProjectButton");
+
+let christmasButton = document.getElementById("christmasProjectButton");
+
+function changeTest() {
+    document.getElementById("test").innerHTML = "test successful";
+}
+
+latestButton.addEventListener('click', changeTest );
+
+
