d0872c36ce68e629e8bfe4a9d244a1f9d4952e19
ylewisbedford <yobedford@gmail.com>
2023-04-10 21:06:02 +0100

    working buttons to switch in desktop mode

--- button.js
+++ button.js
@@ -5,30 +5,57 @@ let plusLate = document.getElementById("plusLatest");
 
 let latestSection = document.getElementById("latest");
 let christmasSection = document.getElementById("christmas");
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+
 
 function unhideLatest(event) {
-    if (latestSection.hidden === true) {
-        latestSection.hidden = false;
-        plusLate.innerHTML = "-";
 
+    if (mediaQuery.matches) {
+
+        if (latestSection.hidden === true) {
+            latestSection.hidden = false;
+            plusLate.innerHTML = "-";
+
+        } else {
+            latestSection.hidden = true;
+            plusLate.innerHTML = "+";
+        }
     } else {
-        latestSection.hidden = true;
-        plusLate.innerHTML = "+";
+        document.getElementById('latest-desktop').style.display = 'block';
+        document.getElementById('christmas-desktop').style.display = 'none';
+        christmasButton.style.background = "#56ff8a";
+        latestButton.style.background = '#3daa5e';
+
     }
+
 }
 
 latestButton.addEventListener('click', unhideLatest);
 
 function unhideChristmas() {
-    if (christmasSection.hidden === true) {
-        christmasSection.hidden = false;
-        plusChristmas.innerHTML = "-";
+
+
+    if (mediaQuery.matches) {
+
+        if (christmasSection.hidden === true) {
+            christmasSection.hidden = false;
+            plusChristmas.innerHTML = "-";
+        } else {
+            christmasSection.hidden = true;
+            plusChristmas.innerHTML = "+";
+        }
+
     } else {
-        christmasSection.hidden = true;
-        plusChristmas.innerHTML = "+";
+        document.getElementById('christmas-desktop').hidden = false;
+        document.getElementById('latest-desktop').style.display = 'none';
+        latestButton.style.background = "#56ff8a";
+        christmasButton.style.background = '#3daa5e';
     }
 }
 
 christmasButton.addEventListener('click', unhideChristmas);
+
+
+
 
 
