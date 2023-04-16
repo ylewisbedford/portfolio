db1f4f19882893591a1f688124aeddc592fd7a4b
ylewisbedford <yobedford@gmail.com>
2023-04-16 20:35:10 +0100

    trial consent banner

--- button.js
+++ button.js
@@ -80,6 +80,46 @@ function getRidOfThings() {
 
 window.addEventListener('resize', getRidOfThings);
 
+// cookie banner
 
+(() => {
+    const getCookie = (name) => {
+        const value = " " + document.cookie;
+        console.log("value", `==${value}==`);
+        const parts = value.split(" " + name + "=");
+        return parts.length < 2 ? undefined : parts.pop().split(";").shift();
+    };
 
+    const setCookie = function (name, value, expiryDays, domain, path, secure) {
+        const exdate = new Date();
+        exdate.setHours(
+            exdate.getHours() +
+            (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
+        );
+        document.cookie =
+            name +
+            "=" +
+            value +
+            ";expires=" +
+            exdate.toUTCString() +
+            ";path=" +
+            (path || "/") +
+            (domain ? ";domain=" + domain : "") +
+            (secure ? ";secure" : "");
+    };
+
+    const $cookiesBanner = document.querySelector(".cookies-eu-banner");
+    const $cookiesBannerButton = $cookiesBanner.querySelector("button");
+    const cookieName = "cookiesBanner";
+    const hasCookie = getCookie(cookieName);
+
+    if (!hasCookie) {
+        $cookiesBanner.classList.remove("hidden");
+    }
+
+    $cookiesBannerButton.addEventListener("click", () => {
+        setCookie(cookieName, "closed");
+        $cookiesBanner.remove();
+    });
+})();
 
